4599598058400f4cd2d3f09fdf377696d1b3f019
DevisedWeb <contact@devisedweb.com>
2026-06-08 04:51:41 +0500

    Testing initial calculation

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(1, 2), subtract(2, 1), multiply(1, 2), divide(1, 2));
