e244605b81862b7f457614a3820dbd25f2df72b3
DevisedWeb <contact@devisedweb.com>
2026-06-08 05:21:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let op;
+let num2;
+
 function add(a, b) {
   return a + b;
 }
@@ -14,4 +18,26 @@ function divide(a, b) {
   return a / b;
 }
 
-console.log(add(1, 2), subtract(2, 1), multiply(1, 2), divide(1, 2));
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      return "Invalid operator";
+  }
+}
+
+console.log(operate("+", 3, 5));
+console.log(operate("-", 10, 4));
+console.log(operate("*", 2, 3));
+console.log(operate("/", 8, 2));
